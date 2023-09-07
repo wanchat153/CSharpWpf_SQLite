53c55c984f1b356e50a1a5140332068a48a4367c
M153 <wanchatgame@gmail.com>
2023-09-07 12:24:24 +0700

    INSERT

--- DataAccess.cs
+++ DataAccess.cs
@@ -17,8 +17,27 @@ namespace CSharpWpf_SQLite
                 String tableCommand = "CREATE TABLE IF NOT " +
                     "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
                     "Text_Entry NVARCHAR(2048) NULL)";
+
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
                 createTable.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        //INSERT INTO MyTable data
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+                insertCommand.ExecuteReader();
+                db.Close();
             }
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace CSharpWpf_SQLite
         {
             InitializeComponent();
             DataAccess.InitializeDatabase();
+            DataAccess.AddData("Wanchat");
         }
     }
 }
